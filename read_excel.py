692b38ac1ef3a5d022036a920cc38311387765b6
yunusarli <yunusarlivdl@gmail.com>
2022-01-29 13:56:58 +0300

    read_excel added

---
+++ read_excel.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from io import StringIO
+from xlsx2csv import Xlsx2csv
+import time
+
+def convert(dataframe):
+    data = dataframe.to_dict(orient="records")
+    return data
+
+def read_excel(path: str) -> pd.DataFrame:
+    start = time.time()
+    buffer = StringIO()
+    Xlsx2csv(path,outputencoding="utf-8").convert(buffer)
+    buffer.seek(0)
+    df = pd.read_csv(buffer)
+    finish = time.time()
+    data = convert(df)
+    return "Donusum Suresi: {}".format(finish - start)
+
+path = 'notlar.xlsx'
+
+
+print(read_excel(path))
+
+
+    
